c3c335e01ee797b9f5eee3b13c328530cc46ba1a
OutRite <60903237+OutRite@users.noreply.github.com>
2020-05-27 15:47:49 -0700

    fixed leibniz, added explanation-ish

--- siia/leibniz.js
+++ siia/leibniz.js
@@ -1,11 +1,22 @@
-function writePi(log) {
-var iter = 1
-var pie = 1 / iter
-while (true) {
-        iter += 2
-        pie -= 1 / iter
-        log("Pi: " + pie*4 + " with 1/" + iter)
-        iter += 2
-        pie += 1 / iter
-        log("Pi: " + pie*4 + " with 1/" + iter)
-}}
+function writePi(log, iter=1, pie=1) {
+	console.log(iter)
+iterpie = calculatePi(log, iter, pie)
+iter = iterpie[0]
+pie = iterpie[1]
+setTimeout(function() {
+	console.log('settimeout')
+	writePi(writeToPage, iter, pie);
+}, 10)
+
+}
+
+function calculatePi(log, iter=1, pie=1) {
+
+iter += 2
+pie -= 1 / iter
+writeToPage("Pi: " + pie*4 + " with 1/" + iter)
+iter += 2
+pie += 1 / iter
+log("Pi: " + pie*4 + " with 1/" + iter)
+return [iter, pie]
+}
